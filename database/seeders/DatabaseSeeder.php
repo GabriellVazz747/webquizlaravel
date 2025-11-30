b7c6c9cfa9a5cce7cade57aed4bf16271a56be20
DhiegoIsmael <dhyego.ismael@gmail.com>
2025-11-30 15:21:49 -0300

    Corrige o caminho de importacao do roteador

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders; // <-- O namespace correto é crucial
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeders.
+     */
+    public function run(): void
+    {
+        // Aqui é onde você chamaria outros seeders (se existirem).
+        // Por enquanto, vamos deixá-lo vazio para o comando funcionar.
+    }
+}
