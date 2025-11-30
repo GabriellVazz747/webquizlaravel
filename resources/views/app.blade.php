c6eb05c5ff1b6d6d6ac4281465eb08f05ba11ece
DhiegoIsmael <dhyego.ismael@gmail.com>
2025-11-30 11:43:25 -0300

    algumas atualizações e  correções

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
-    @vite('resources/js/app.js')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Web Quiz</title>
+    
+    @vite(['resources/js/main.js', 'resources/js/assets/style.css'])
 </head>
 <body>
-    <div id="app"></div>
+    
+    <div id="app"></div> 
+    
 </body>
 </html>
